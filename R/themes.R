8bb1deccd2bf35d40276033c90413b05191a5866
conig <james.conigrave@gmail.com>
2021-10-27 14:32:50 +1100

    add synthwave

--- R/themes.R
+++ R/themes.R
@@ -9,3 +9,15 @@ bluebell = function() {
   rstudioapi::addTheme(system.file("bluebell.rstheme", package = "JTheme"), apply = TRUE)
   cat(crayon::blue("---Bluebell---"))
 }
+
+#' synthwave84
+#' @export
+
+synthwave84 = function() {
+  try(suppressWarnings(rstudioapi::removeTheme("synthwave84")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("synthwave84.rstheme", package = "JTheme"), apply = TRUE)
+  cat("---synthwave84---")
+  cat(crayon::blue("This is a performance tweak of an existing theme. See the original more glowy theme here: https://github.com/jnolis/synthwave85"))
+
+}
