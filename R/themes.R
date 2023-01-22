47c0e31d3108682c209068acedd43d41d00ba3dd
conig <james.conigrave@gmail.com>
2023-01-22 16:35:11 +1100

    add additional themes

--- R/themes.R
+++ R/themes.R
@@ -10,14 +10,88 @@ bluebell = function() {
   cat(crayon::blue("---Bluebell---"))
 }
 
-#' synthwave84
+#' synthwave85
 #' @export
 
-synthwave84 = function() {
+synthwave85 = function(simple = FALSE) {
   try(suppressWarnings(rstudioapi::removeTheme("synthwave84")),
       silent = TRUE)
+
+  if(simple){
+      try(suppressWarnings(rstudioapi::removeTheme("Synthwave84")),
+      silent = TRUE)
   rstudioapi::addTheme(system.file("synthwave84.rstheme", package = "JTheme"), apply = TRUE)
-  cat("---synthwave84---")
-  cat(crayon::blue("This is a performance tweak of an existing theme. See the original more glowy theme here: https://github.com/jnolis/synthwave85"))
+
+    }else{
+        try(suppressWarnings(rstudioapi::removeTheme("Synthwave85")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("synthwave85.rstheme", package = "JTheme"), apply = TRUE)
+  }
+
+  cat("---synthwave84---\n")
+  cat(crayon::blue("Modified from: https://github.com/jnolis/synthwave85\n"))
 
 }
+
+
+#' Nineties_BIOS
+#' @export
+
+Nineties_BIOS = function() {
+  try(suppressWarnings(rstudioapi::removeTheme("Nineties BIOS")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("90s/Nineties_BIOS.rstheme", package = "JTheme"), apply = TRUE)
+  cat("\n---Nineties BIOS---\n")
+  cat(crayon::blue("CREDIT: https://github.com/jaredgorski/Nineties\n"))
+
+}
+
+
+#' Nineties
+#' @export
+
+Nineties = function() {
+  try(suppressWarnings(rstudioapi::removeTheme("Nineties")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("90s/Nineties.rstheme", package = "JTheme"), apply = TRUE)
+  cat("\n---Nineties---\n")
+  cat(crayon::blue("CREDIT: https://github.com/jaredgorski/Nineties\n"))
+
+}
+
+#' Nineties_bigblue
+#' @export
+
+Nineties_bigblue = function() {
+  try(suppressWarnings(rstudioapi::removeTheme("Nineties Big Blue")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("90s/BigBlue.rstheme", package = "JTheme"), apply = TRUE)
+  cat("\n---Nineties Big Blue---\n")
+  cat(crayon::blue("CREDIT: https://github.com/jaredgorski/Nineties\n"))
+
+}
+
+#' Nineties_apple
+#' @export
+
+Nineties_apple = function() {
+  try(suppressWarnings(rstudioapi::removeTheme("Nineties Apple Light")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("90s/Apple.rstheme", package = "JTheme"), apply = TRUE)
+  cat("\n---Nineties Apple Light---\n")
+  cat(crayon::blue("CREDIT: https://github.com/jaredgorski/Nineties\n"))
+
+}
+
+#' Nineties_appledark
+#' @export
+
+Nineties_appledark = function() {
+  try(suppressWarnings(rstudioapi::removeTheme("Nineties Apple Dark")),
+      silent = TRUE)
+  rstudioapi::addTheme(system.file("90s/Appledark.rstheme", package = "JTheme"), apply = TRUE)
+  cat("\n---Apple Dark---\n")
+  cat(crayon::blue("CREDIT: https://github.com/jaredgorski/Nineties\n"))
+
+}
+
